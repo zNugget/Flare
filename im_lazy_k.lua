ac4e8280d18e6d391419ff98f1a94581dad79b59
zNugget <81307155+zNugget@users.noreply.github.com>
2021-12-08 21:35:57 -0500

    Create im_lazy_k.lua

---
+++ im_lazy_k.lua
@@ -0,0 +1,9 @@
+game.StarterGui:SetCore("SendNotification", {
+        Title = "AutoCase"; 
+        Text = "AutoCase Is Now Active."; 
+        Icon = ""; 
+        Duration = 5;
+        Callback = bindableFunction;
+        Button1 = "";  
+        Button2 = "";
+    })
